cdd94513292ff5de538727da14e6e45bca15950c
MFBuckley <m.buckleyeos@gmail.com>
2020-09-27 12:02:38 +0000

    Added checkout views and templates

--- marybees/settings.py
+++ marybees/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
-    
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -59,6 +59,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'marybees.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,8 +76,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
