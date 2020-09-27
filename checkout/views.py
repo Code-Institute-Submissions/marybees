5186810a3e4dc9e2be2a732b91e1c4f79711713e
MFBuckley <m.buckleyeos@gmail.com>
2020-09-27 12:48:28 +0000

    Added stripe form submission logic

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.conf import settings
-
 from .forms import OrderForm
 from bag.contexts import bag_contents
-
 import stripe
 
 
